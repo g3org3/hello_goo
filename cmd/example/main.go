e355eb7428704d6f4bbb7b4ef209797dd1f5f4fb
george <7jagjag@gmail.com>
2026-03-08 03:36:12 +0100

    feat: interactive TUI with GitHub username lookup using charmbracelet/huh

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,12 +1,81 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 
-	"github.com/g3org3/hello_goo"
+	hello "github.com/g3org3/hello_goo"
+
+	"github.com/charmbracelet/huh"
 )
 
+type githubUser struct {
+	Name  string `json:"name"`
+	Login string `json:"login"`
+}
+
+func fetchGitHubName(username string) (string, error) {
+	resp, err := http.Get("https://api.github.com/users/" + username)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("GitHub API returned status %d", resp.StatusCode)
+	}
+
+	var user githubUser
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		return "", err
+	}
+
+	if user.Name != "" {
+		return user.Name, nil
+	}
+	return user.Login, nil
+}
+
 func main() {
-	fmt.Println(hello_goo.Hello("yeah"))
-	fmt.Println(hello_goo.Formal("World"))
+	var username string
+	var style string
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("What's your GitHub username?").
+				Placeholder("g3org3").
+				Value(&username),
+		),
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Greeting style").
+				Options(
+					huh.NewOption("Casual", "casual"),
+					huh.NewOption("Formal", "formal"),
+				).
+				Value(&style),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		fmt.Println("Cancelled.")
+		os.Exit(0)
+	}
+
+	name, err := fetchGitHubName(username)
+	if err != nil {
+		fmt.Printf("Could not fetch GitHub profile for %q, using username instead.\n", username)
+		name = username
+	}
+
+	switch style {
+	case "formal":
+		fmt.Println(hello.Formal(name))
+	default:
+		fmt.Println(hello.Hello(name))
+	}
 }
